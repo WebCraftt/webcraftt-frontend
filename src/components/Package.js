e504e9a8abad9b63dbfc217bff92fb923d3553a0
Shaunak KD <sagardeyyash19@gmail.com>
2023-07-30 14:05:45 +0600

    update components

---
+++ src/components/Package.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PackageItem from './PackageItem';
+
+const Package = () => {
+  return (
+    <div>
+        <h1>Package</h1>
+      <PackageItem/>
+    </div>
+  )
+}
+
+export default Package;
---
+++ src/components/PackageItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PackageItem = () => {
+  return (
+    <div>
+      <h1>Package Item</h1>
+    </div>
+  )
+}
+
+export default PackageItem;
