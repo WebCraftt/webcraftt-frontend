b3755ded7a4384a2cdf2700dfdcadc05357b1032
Shaunak KD <sagardeyyash19@gmail.com>
2023-07-31 14:22:36 +0600

    Update the navbar

---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import AdminLayout from '@/src/layout/AdminLayout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <AdminLayout>
+      <h1>Admin</h1>
+    </AdminLayout>
+  )
+}
+
+export default index;
+
---
+++ src/layout/AdminLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const AdminLayout = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default AdminLayout;
