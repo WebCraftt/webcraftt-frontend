e67d8871107f8827781a67d945c17c99249a7a27
iamshujasaqib <iamshujasaqib@gmail.com>
2023-08-15 22:55:51 +0500

    Refreshing Code for Facebook Messenger Integration

--- src/components/FacebookMsg.js
+++ src/components/FacebookMsg.js
@@ -5,7 +5,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 function FacebookMsg() {
   return (
     <FacebookProvider appId="664032472040233" chatSupport>
-      <CustomChat pageId="122104685528003562" minimized="true" />
+      <CustomChat pageId="122104685528003562" minimized={true} />
     </FacebookProvider>
   );
 }
