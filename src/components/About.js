4fca269b2f6755d373af7418d8a002c804062e1f
codeftshaunak <sagardeyyash19@gmail.com>
2023-08-22 16:09:41 +0600

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,28 +4,28 @@ import Image from "next/image";
 const About = () => {
   return (
     <div
-      className="bg-white dark:bg-gray-900 h-screen flex justify-center items-center"
+      className=" bg-gray-900 h-screen flex justify-center items-center"
       id="HeroPage"
     >
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <div data-aos="fade-right">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
               Make Your Business One Step Higher With{" "}
               <span className="text-[#9dd4e8]">WebCraft</span>
             </h1>
           </div>
           <p
             data-aos="zoom-in"
-            className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+            className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400"
           >
             We help people get their business to the next level!
           </p>
           <a
             href="#"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 mr-3"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border rounded-lg focus:ring-4  border-gray-700 hover:bg-gray-700 focus:ring-gray-800 mr-3"
           >
-            Get started
+            Get started 
             <svg
               className="w-5 h-5 ml-2 -mr-1"
               fill="currentColor"
@@ -38,12 +38,6 @@ const About = () => {
                 clipRule="evenodd"
               ></path>
             </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          >
-            Start Explore
           </a>
         </div>
         <div
